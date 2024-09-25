a6b01035d819f23d335128d890b9699f3e6b8f40
TheDrifter408 <nurullahan408@gmail.com>
2024-09-25 20:07:36 +0600

    caesar_cipher solved

---
+++ script.rb
@@ -0,0 +1,28 @@
+def shifted(nums, shift_val = 5)
+  if nums >= 32  and nums <= 64
+    return nums
+  end
+  new_num = nums + shift_val;
+  if nums >= 65 and nums <= 90
+    if (new_num % 90) == new_num
+      return new_num
+    else 
+      new_num = new_num % 90 + 64
+    end
+  elsif nums >= 97 and nums <= 122
+    if (new_num % 122) == new_num
+      return new_num
+    else 
+      new_num = new_num % 122 + 96
+    end
+  end
+  return new_num
+end
+
+def caesar_cipher(input_str,shift_val)
+  byte_nums = input_str.bytes.map { |byte| shifted(byte,shift_val) }
+  new_str = byte_nums.map { |byte| byte.chr }.join("")
+  return new_str;
+end
+
+puts caesar_cipher("What a string!",5)
